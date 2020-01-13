1d926946aebbf36b8845f5bd40c846eaaa2b0ca5
galahad42 <59364507+galahad42@users.noreply.github.com>
2020-01-14 03:35:32 +0530

    Add files via upload

---
+++ 2048/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='2048',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='galahad42',
+    author_email='amritanand1407@gmail.com',
+    description='This  is a python program for 2048 game.'
+)
