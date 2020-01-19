c8035e47474e79093768eaa0be13ff3087dc939f
galahad42 <59364507+galahad42@users.noreply.github.com>
2020-01-19 08:28:33 +0530

    Update div.py

--- Divisors/div.py
+++ Divisors/div.py
@@ -1,6 +1,6 @@
 import math
 
-def div_gen(n):
+def div(n):
     l_div= int(math.sqrt(n) + 1)
     lrg_div = []
     for i in range(1, l_div):  # As the divsior of any natural number 'n' have range from 1 to (square root of n)+1
@@ -11,4 +11,4 @@ def div_gen(n):
     for divisor in reversed(lrg_div):
         yield divisor
 n = int(input(""))
-print(len(list(div_gen(n))))
+print(len(list(div(n))))
