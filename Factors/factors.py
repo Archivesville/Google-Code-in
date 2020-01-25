cc98aa99016a26913291ddd380e82f24aeaa53f4
galahad42 <59364507+galahad42@users.noreply.github.com>
2020-01-25 21:42:20 +0530

    Update factors.py

--- Factors/factors.py
+++ Factors/factors.py
@@ -1,46 +1,13 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from spellchecker import SpellChecker
-import re
+n = int(input("Enter a no.: "))
 
+factors = []
+for i in range(1, int(n ** 0.5) + 1):
+    if n % i == 0:
+        factors.append(i)
+        other_factor = int(n / i)
+      
+        if i != other_factor:
+            factors.append(other_factor)
 
-spellings_to_check = ["hello", "how", "what", "however", "something", "anything",
-                    "give", "day", "about", "time", "make", "just", "people", "good", 
-                    "could", "would", "think", "after", "work", "because"]
-
-# Checks the word if it is in the checkSpellings list. If so, it corrects it.
-def check(word):
-    global spellings_to_check
-    
-    spell = SpellChecker()
-    word = word.lower()
-    
-    for possible in spell.candidates(word):
-        if possible in spellings_to_check:
-            return possible
-    
-    return word        
-
-
-# shows list of words that shall be checked
-print()
-print("The spellings will be checked for the following words:")
-for a in spellings_to_check:
-    if a != spellings_to_check[-1]:
-        print(a, end=", ")
-    else:
-        print("\n")
-
-
-# Asks for input
-input_para = input("Enter the paragraph you want to check>> ")
-print()
-paragraph =  re.split('[\s]', input_para)
-
-
-# Prints out the corrected paragraph
-for word in paragraph:
-    a = check(word)
-    print(a + " ", end="")
-
-print()
+str_factors = ", ".join(list(map(str, sorted(factors))))
+print(f"\n{n} has {len(factors)} factors:\n\n{str_factors}")
